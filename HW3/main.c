7555f35629656f56de85aa050aec4859598f58ec
Cole Abbott <caskisail18@icloud.com>
2023-04-05 18:09:09 -0500

    HW3

--- HW3/main.c
+++ HW3/main.c
@@ -1,5 +1,5 @@
 #include "nu32dip.h" // constants, functions for startup and UART
-
+#include <math.h>
 
 int main(void) {
     char message[200];
@@ -10,14 +10,17 @@ int main(void) {
         while(NU32DIP_USER == 1) {}
             for (int i = 0; i < 100; i++){
                 //write i to uart
-                sprintf(message, "%d\n\r", i);
+                //sprintf(message, "%d\n\r", i);
+                //NU32DIP_WriteUART1(message);
+
+                //write sine wave with 100 samples to UART
+                sprintf(message, "%f\n\r", (3.3* sin(2*3.14159*i/100)));
                 NU32DIP_WriteUART1(message);
 
                 //wait 0.01 seconds
                 time = _CP0_GET_COUNT();
                 while(_CP0_GET_COUNT() - time < 240000) {}
             }
-    
     }
 }
 
