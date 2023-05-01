1aab005ad5c298f6cfb4b82b0b68276c9de055e3
Cole Abbott <caskisail18@icloud.com>
2023-05-01 11:18:20 -0500

    HW9

---
+++ HW9/cvs_fft.py
@@ -0,0 +1,39 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = [] # column 0
+data = [] # column 1
+
+with open('sigA.csv') as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows 1 one by one
+        t.append(float(row[0])) # leftmost column
+        data.append(float(row[1])) # second column
+
+
+y = data # the data to make the fft from
+n = len(y) # length of the signal
+
+Fs = n/t[-1] # sample rate
+Ts = 1.0/Fs; # sampling interval
+ts = np.arange(0,t[-1],Ts) # time vector 
+k = np.arange(n)
+T = n/Fs
+frq = k/T # two sides frequency range
+frq = frq[range(int(n/2))] # one side frequency range
+Y = np.fft.fft(y)/n # fft computing and normalization
+Y = Y[range(int(n/2))]
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+ax1.plot(t,y,'b')
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Amplitude')
+ax2.loglog(frq,abs(Y),'b') # plotting the fft
+ax2.set_xlabel('Freq (Hz)')
+ax2.set_ylabel('|Y(freq)|')
+plt.show()
